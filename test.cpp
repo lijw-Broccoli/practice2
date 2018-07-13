538de34052fd65df54a989ec46fb261473b2bdae
star-lee <lijw@leadsec.com.cn>
2018-07-12 21:26:22 -0400

    initialize the class function

--- test.cpp
+++ test.cpp
@@ -2,6 +2,19 @@
 #include <string>
 #include "test.h"
 using namespace std;
+Person::person()
+{
+   cout<<"Person()"<<endl;
+}
+Person::person(const string &t_name,int t_age)
+  :m_name(t_name),m_age(t_age)
+  {
+    cout<<"person(...)"<<endl;
+  }
+void Person::print()
+{
+  cout<<"name: "<< m_name<<" age: "<<m_age<<endl;
+}
 int main()
 {
   cout<<"hell world!"<<endl;
