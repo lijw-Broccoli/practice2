ec03e21714e29d46246151787be6b768d4acdf42
star-lee <lijw@leadsec.com.cn>
2018-07-12 21:31:56 -0400

    Update test.cpp
    initialize the function

--- test.cpp
+++ test.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 Person::person()
 {
+   m_name = "jim";
+   m_age = 20;
    cout<<"Person()"<<endl;
 }
 Person::person(const string &t_name,int t_age)
@@ -14,6 +16,14 @@ Person::person(const string &t_name,int t_age)
 void Person::print()
 {
   cout<<"name: "<< m_name<<" age: "<<m_age<<endl;
+}
+string Person::getName()
+{
+   return m_name;
+}
+int Person::getAge()
+{
+   return m_age;
 }
 int main()
 {
